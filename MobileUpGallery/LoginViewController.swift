3968652f0ac8825dd613dd1a6b573b4e056dd929
Lazzatios <lseiilova@nu.edu.kz>
2021-07-27 17:26:02 +0600

    feat: add login view controller .swift and .xib

---
+++ MobileUpGallery/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MobileUpGallery
+//
+//  Created by Lazzat Seiilova on 27.07.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
