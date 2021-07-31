1500f40f58a38d9f0106167276c69545a736606b
Lazzatios <lseiilova@nu.edu.kz>
2021-07-31 17:06:29 +0600

    feat: add extension to download image from its url

--- MobileUpGallery/AlbumViewController/AlbumViewController.swift
+++ MobileUpGallery/AlbumViewController/AlbumViewController.swift
@@ -59,3 +59,24 @@ class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollect
         return CGSize(width: sideSize, height: sideSize)
     }
 }
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFill) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFill) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
